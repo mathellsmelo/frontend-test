4a546bd65ffe387cf40e829f777fc085b1b24f0b
Matheus Melo] <matheus.melo@innovapost.com>
2021-12-08 14:40:24 -0500

    adicionando url de teste

--- src/contants.js
+++ src/contants.js
@@ -1,3 +1,3 @@
-export const API_BASE_URL = 'http://localhost:8080/api';
+export const API_BASE_URL = 'http://backend-test-dev-service0.integration-matheus.svc:8080/api';
 export const ACCESS_TOKEN = 'accessToken';
 export const ROLE = '';
